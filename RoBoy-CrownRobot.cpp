40b5d2951945172053659079fa9e6e780b886cd1
ppjj5566 <ppjj5566@naver.com>
2024-12-03 16:11:45 +0900

    kinematics are partialy done

--- RoBoy-CrownRobot.cpp
+++ RoBoy-CrownRobot.cpp
@@ -1,7 +1,6 @@
 #include <stdio.h>
 #include "pico/stdlib.h"
 #include "pico/multicore.h"
-#include "hardware/adc.h"
 #include "tusb.h"
 
 #include "wifi_connection.cpp"
@@ -14,22 +13,22 @@ using namespace servo;
 
 char ssid[64], pw[64];
 const int port = 12345;
-volatile float x, y, z;
 float v_sersor_value = 0.194579f;
 
 const uint START_PIN = servo2040::SERVO_1;
 const uint END_PIN = servo2040::SERVO_18;
 const uint NUM_SERVOS = (END_PIN - START_PIN) + 1;
-Servo *servos[NUM_SERVOS];
 Calibration *calibration[NUM_SERVOS];
-ServoCluster *servo_cluster; 
+ServoCluster *servo_cluster;
 
 wifi_connection wifi;
 udp_server server;
 static Kinematics *kinematics1, *kinematics2, *kinematics3, *kinematics4, *kinematics5, *kinematics6;
 received_joystick_data joy_data = {0, 0, 0, 0, 0, 0, 0, 0, 0};
 
-const float sin60 = 0.86602540378f;                                                                                                                                                                                                                                                                                                                                                     
+const float sin60 = 0.86602540378f;
+const float sin30 = 0.5f;
+const float cos30 = 0.86602540378f;                                                                                                                                                                                                                                                                                                                                           
 const float cos60 = 0.5f;
 
 void get_char_from_tinyusb(char *buffer){
@@ -63,35 +62,54 @@ void get_and_send_char_to_tinyusb(char *buffer, const char *message){
     send_char_to_tinyusb(message);
 }
 
+// bool repeat_voltage_measurement(struct repeating_timer *t){
+//     adc_init();
+//     adc_gpio_init(26);
+//     adc_select_input(0);
+//     uint16_t result = adc_read() * v_sersor_value;
+//     const float conversion_factor = 3.3f / (1 << 12);
+//     char buffer[30];
+//     sprintf(buffer, "Voltage: %f\n", conversion_factor * result);
+//     send_char_to_tinyusb(buffer);
+//     return true;
+// }
+
 void core1_entry(){
     while (true)
     {
+        volatile float x, y, z;
+
         received_joystick_data *recv_joy_data = (received_joystick_data *)multicore_fifo_pop_blocking();
-        multicore_fifo_drain();
-
+        
         x = recv_joy_data->x1;
-        y = 50.0f + recv_joy_data->y1;
-        z = -30.0f + recv_joy_data->z1;
+        y = recv_joy_data->y1;
+        z = -20.0f + recv_joy_data->z1;
 
-        kinematics1->endpoint(x, y, z);
-        kinematics2->endpoint(x, y, z);
-        kinematics3->endpoint(x, y, z);
-        kinematics4->endpoint(x, y, z);
-        kinematics5->endpoint(x, y, z);
-        kinematics6->endpoint(x, y, z);
+        kinematics1->endpoint((cos60 * x) + (sin60 * y), (50.0f + (cos60 * y)) - (sin60 * x), z);
+        kinematics2->endpoint(x, 50.0f + y, z);
+        kinematics3->endpoint((cos60 * x) - (sin60 * y), (50.0f + (cos60 * y)) + (sin60 * x), z);
+        kinematics4->endpoint((cos60 * -x) - (sin60 * y), (50.0f - (cos60 * y)) + (sin60 * x), z);
+        kinematics5->endpoint(-x, 50.0f - y, z);
+        kinematics6->endpoint((cos60 * -x) + (sin60 * y), (50.0f - (cos60 * y)) - (sin60 * x), z);
+
+        multicore_fifo_drain();
     }
 }
 
 int main(){
+    // alarm_pool_t *alarm_pool;
+    // repeating_timer_t *t;
+
     stdio_init_all();
     tusb_init();
     multicore_reset_core1();
     multicore_launch_core1(core1_entry);
+    // alarm_pool_add_repeating_timer_ms(alarm_pool, 200, repeat_voltage_measurement, NULL, t); // stoping hole program
 
     sleep_ms(3000);
     send_and_get_char_from_tinyusb("Enter SSID: ", ssid);
     send_and_get_char_from_tinyusb("Enter Password: ", pw);
-
+    
     servo_cluster = new ServoCluster(pio0, 0, START_PIN, NUM_SERVOS);
     servo_cluster->init();
     servo_cluster->enable_all();
@@ -100,18 +118,6 @@ int main(){
     {
         servo_cluster->calibration(i).apply_three_pairs(460.0f, 1430.0f, 2400.0f, 0.0f, 90.0f, 180.0f);
     }
-    
-
-    // for(auto s = 0u; s < NUM_SERVOS; s++) {
-    //     servos[s] = new Servo(s + START_PIN);
-    //     servos[s]->init();
-    // }
-
-    // for(auto s = 0u; s < NUM_SERVOS; s++) {
-    //     calibration[s] = &servos[s]->calibration();
-    //     calibration[s]->apply_three_pairs(460.0f, 1430.0f, 2400.0f, 0.0f, 90.0f, 180.0f);
-    //     servos[s]->enable();
-    // }
 
     kinematics1 = new Kinematics(servo_cluster, 0);
     kinematics2 = new Kinematics(servo_cluster, 1);
